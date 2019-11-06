430294bece9e220b4a24c18a8cabda8760429acd
Stefan Mavrodiev <stefan.mavrodiev@gmail.com>
2019-11-06 23:48:50 +0200

    Add submodules

--- nominatim/__main__.py
+++ nominatim/__main__.py
@@ -3,6 +3,7 @@ import re
 
 import click
 import urllib3
+import json
 
 from .formatters import Formatter
 from .validators import Validators
@@ -15,38 +16,93 @@ http = urllib3.PoolManager()
 @click.command()
 @click.argument("output")
 def cli(**kwargs):
-    ret: urllib3.response.HTTPResponse = http.request(
-        'GET',
-        "https://raw.githubusercontent.com/openstreetmap/Nominatim/master/data/country_name.sql"
-    )
 
-    data: str = ret.data.decode()
-    # print(data)
+    countries = {}
+    names = {}
 
-    # Parse data
-    formatter = Formatter.json(format={
-        'id': str,
-        'geojson': str,
-        'name_en': str,
-        'name_bg': str,
-    })
+    # Load country list
+    with open("externals/country-list/data/en/country.json", 'r') as f:
+        names['en'] = json.load(f)
 
-    for line in data.split('\n'):
-        if '\"name\"=>' not in line:
-            continue
+    with open("externals/country-list/data/bg/country.json", 'r') as f:
+        names['bg'] = json.load(f)
 
-        country = {}
+    # Compare with countries list from nominatim
+    temp = []
+    with open("externals/nomatium/data/country_name.sql") as f:
+        for line in f.readlines():
+            if '"name"=>' not in line:
+                continue
 
-        # Get country code
-        code = line.split()[0]
-        line = line[len(code):]
-        print(line)
-        print(line.split(','))
-        Validators.country_code(code)
+            # Get country code
+            code = line.split()[0]
+            line = line[len(code):]
+            try:
+                Validators.country_code(code)
+            except ValueError:
+                continue
+
+            data = {}
+
+            if code in [x.lower() for x in list(names['en'].keys())]:
+                data['name_en'] = names['en'][code.upper()]
+            else:
+                for name in line.split(', '):
+                    if '"name"=>' in name:
+                        data['name_en'] = name.split('=>')[1].replace('"', '')
+                    elif '"name:en"=>' in name:
+                        data['name_en'] = name.split('=>')[1].replace('"', '')
+
+            if code in [x.lower() for x in list(names['bg'].keys())]:
+                data['name_bg'] = names['bg'][code.upper()]
+            else:
+                for name in line.split(', '):
+                    if '"name"=>' in name:
+                        data['name_bg'] = name.split('=>')[1].replace('"', '')
+                    elif '"name:bg"=>' in name:
+                        data['name_bg'] = name.split('=>')[1].replace('"', '')
+
+            countries[code] = data
+            print(countries)
+            break
 
 
 
-        return
+
+
+    # ret: urllib3.response.HTTPResponse = http.request(
+    #     'GET',
+    #     "https://raw.githubusercontent.com/openstreetmap/Nominatim/master/data/country_name.sql"
+    # )
+    #
+    # data: str = ret.data.decode()
+    # # print(data)
+    #
+    # # Parse data
+    # formatter = Formatter.json(format={
+    #     'id': str,
+    #     'geojson': str,
+    #     'name_en': str,
+    #     'name_bg': str,
+    # })
+    #
+    # for line in data.split('\n'):
+    #     if '\"name\"=>' not in line:
+    #         continue
+    #
+    #     country = {}
+    #
+    #     # Get country code
+    #     code = line.split()[0]
+    #     line = line[len(code):]
+    #     print(line)
+    #     for l in line.split(', '):
+    #         print(l)
+    #     Validators.country_code(code)
+    #
+    #
+    #
+    #     return
 
 
 
