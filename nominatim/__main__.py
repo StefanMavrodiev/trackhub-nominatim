07490d892aa029b9e139327976763c6dead1b90b
Stefan Mavrodiev <stefan.mavrodiev@gmail.com>
2019-11-08 19:45:15 +0200

    Provide excluded list countries

--- nominatim/__main__.py
+++ nominatim/__main__.py
@@ -1,14 +1,13 @@
 import click
 import urllib3
 import json
+import time
 
 from .formatters import Formatter
 from .validators import Validators
 
 # https://raw.githubusercontent.com/openstreetmap/Nominatim/master/data/country_name.sql
-# https://nominatim.openstreetmap.org/search?country=bulgaria&format=geojson&polygon_geojson=1
-
-http = urllib3.PoolManager()
+# https://nominatim.openstreetmap.org/search?country=bulgaria&format=geojson&polygon_geojson=0
 
 
 @click.command()
@@ -32,15 +31,18 @@ def cli(**kwargs):
                 continue
 
             # Get country code
-            code = line.split()[0]
-            line = line[len(code):]
+            code = line.split('\t')[0]
+            line = line.split('\t')[1]
             try:
                 Validators.country_code(code)
+                Validators.country_exclude(code)
             except ValueError:
                 continue
 
+
             data = {}
 
+            # Get english name
             if code in [x.lower() for x in list(names['en'].keys())]:
                 data['name_en'] = names['en'][code.upper()]
             else:
@@ -49,7 +51,9 @@ def cli(**kwargs):
                         data['name_en'] = name.split('=>')[1].replace('"', '')
                     elif '"name:en"=>' in name:
                         data['name_en'] = name.split('=>')[1].replace('"', '')
+                print("Missing country in country-list/en: \'{}\'. Using: \'{}\'".format(code, data['name_en']))
 
+            # Get bulgarian name
             if code in [x.lower() for x in list(names['bg'].keys())]:
                 data['name_bg'] = names['bg'][code.upper()]
             else:
@@ -58,20 +62,39 @@ def cli(**kwargs):
                         data['name_bg'] = name.split('=>')[1].replace('"', '')
                     elif '"name:bg"=>' in name:
                         data['name_bg'] = name.split('=>')[1].replace('"', '')
-
-            countries[code] = data
-
-    print(countries)
+                print("Missing country in country-list/bg: \'{}\'. Using: \'{}\'".format(code, data['name_en']))
 
 
+            # Get geojson
+            http = urllib3.PoolManager(headers={
+                'User-Agent': '	Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0'
+            })
+            ret: urllib3.response.HTTPResponse = http.request(
+                'GET',
+                "https://nominatim.openstreetmap.org/search?country={}&format=geojson&polygon_geojson=0".format(
+                    data['name_en'].replace(' ', '%20').replace('&', 'and').lower()
+                )
+            )
+
+            print("{} : {} -> {}".format(code, data['name_en'], data['name_bg']))
+            j = json.loads(ret.data.decode())
+
+            if len(j['features']) == 0:
+                print("Missing features!")
+                continue
+
+            if len(j['features']) > 1:
+                for feature in j['features']:
+                    name=feature['properties']['display_name'].lower()
+                    if name != data['name_en'].lower():
+                        j['features'].remove(feature)
+                        continue
+
+            data['geojson'] = j
+            time.sleep(1)
 
 
 
-    # ret: urllib3.response.HTTPResponse = http.request(
-    #     'GET',
-    #     "https://raw.githubusercontent.com/openstreetmap/Nominatim/master/data/country_name.sql"
-    # )
-    #
     # data: str = ret.data.decode()
     # # print(data)
     #
--- nominatim/validators/validators.py
+++ nominatim/validators/validators.py
@@ -1,5 +1,7 @@
 from .country_code import CountryCode
+from .country_exclude import CountryExclude
 
 
 class Validators(object):
     country_code = CountryCode.validate
+    country_exclude = CountryExclude.validate
